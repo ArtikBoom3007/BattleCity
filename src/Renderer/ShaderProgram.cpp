00502c11f49bac2460b2d9f1bf2c2487fdabcc12
Artem Kravch <krav514@gmail.com>
2022-11-06 00:00:25 +0300

    Shader class

---
+++ src/Renderer/ShaderProgram.cpp
@@ -0,0 +1,84 @@
+#include "ShaderProgram.h"
+#include <iostream>
+
+namespace Renderer {
+	ShaderProgram::ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader)
+	{
+		GLuint VertexShaderID;
+		if (!createShader(vertexShader, GL_VERTEX_SHADER, VertexShaderID)) {
+			std::cerr << "VERTEX SHADER compile time error" << std::endl;
+			return;
+		}
+
+		GLuint FragmentShaderID;
+		if (!createShader(fragmentShader, GL_FRAGMENT_SHADER, FragmentShaderID)) {
+			std::cerr << "FRAGMENT SHADER compile time error" << std::endl;
+			glDeleteShader(VertexShaderID);
+			return;
+		}
+
+		m_ID = glCreateProgram();
+		glAttachShader(m_ID, VertexShaderID);
+		glAttachShader(m_ID, FragmentShaderID);
+		glLinkProgram(m_ID);
+
+		GLint success;
+		glGetProgramiv(m_ID, GL_LINK_STATUS, &success);
+		if (!success) {
+			GLchar infoLog[1024];
+			glGetShaderInfoLog(m_ID, 1024, nullptr, infoLog);
+			std::cerr << "ERROR::SHADER: Link time error : \n" << infoLog << std::endl;
+		}
+		else {
+			m_isCompiled = true;
+		}
+
+		glDeleteShader(VertexShaderID);
+		glDeleteShader(FragmentShaderID);
+	}
+
+	bool ShaderProgram::createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID) {
+		shaderID = glCreateShader(shaderType);
+		const char* code = source.c_str();
+		glShaderSource(shaderID, 1, &code, nullptr);
+		glCompileShader(shaderID);
+
+		GLint success;
+		glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			GLchar infoLog[1024];
+			glGetShaderInfoLog(shaderID, 1024, nullptr, infoLog);
+			std::cerr << "ERROR::SHADER: Compile time error : \n" << infoLog << std::endl;
+			return false;
+		}
+		return true;
+	}
+
+	void ShaderProgram::use() const {
+		glUseProgram(m_ID);
+	}
+
+	ShaderProgram& ShaderProgram::operator=(ShaderProgram&& shaderProgram) noexcept {
+		glDeleteProgram(m_ID);
+		m_ID = shaderProgram.m_ID;
+		m_isCompiled = shaderProgram.m_isCompiled;
+
+		shaderProgram.m_ID = 0;
+		shaderProgram.m_isCompiled = false;
+		return *this;
+	}
+	ShaderProgram::ShaderProgram(ShaderProgram&& shaderProgram) noexcept {
+		m_ID = shaderProgram.m_ID;
+		m_isCompiled = shaderProgram.m_isCompiled;
+
+		shaderProgram.m_ID = 0;
+		shaderProgram.m_isCompiled = false;
+	}
+
+	ShaderProgram::~ShaderProgram()
+	{
+		glDeleteProgram(m_ID);
+
+	}
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,44 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include<iostream>
+#include <iostream>
+
+#include "Renderer/ShaderProgram.h"
+
+const char* vertex_shader =
+"#version 430\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main() {"
+"   color = vertex_color;"
+"  gl_Position = vec4(vertex_position, 1.0);"
+"}";
+
+const char* fragment_shader = 
+"#version 430\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main() {"
+ "   frag_color = vec4(color, 1.0);"
+"}";
+
+
 
 int g_WindowSizeX = 640;
 int g_WindowSizeY = 480;
+
+GLfloat point[] = {
+     0.0f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+GLfloat colors[] = {
+     1.0f, 0.0f, 0.0f,
+     0.0f, 1.0f, 0.0f,
+     0.0f, 0.0f, 1.0f
+};
 
 void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height) {
     g_WindowSizeX = width;
@@ -19,17 +53,20 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
     }
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        std::cout << "glfwInit failed" << std::endl;
         return -1;
+    }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    /* Create a windowed mode window and its OpenGL context */
-   GLFWwindow* pWindow = glfwCreateWindow(g_WindowSizeX, g_WindowSizeY, "Battle City", NULL, NULL);
+
+    /* Create a windowed mode pWindow and its OpenGL context */
+    GLFWwindow* pWindow = glfwCreateWindow(g_WindowSizeX, g_WindowSizeY, "Battle City", NULL, NULL);
     if (!pWindow)
     {
         std::cout << "glfwCreateWindow failed!" << std::endl;
@@ -41,30 +78,65 @@ int main(void)
     glfwSetKeyCallback(pWindow, glfwKeyCallback);
     /* Make the pWindow's context current */
     glfwMakeContextCurrent(pWindow);
-	
-	if (!gladLoadGL()) {
+
+    if (!gladLoadGL()) {
         std::cout << "Can't load GLAD!" << std::endl;
         return -1;
     }
     std::cout << "Renderer : " << glGetString(GL_RENDERER) << std::endl;
     std::cout << "OpenGL version : " << glGetString(GL_VERSION) << std::endl;
-	
-	glClearColor(0, 1, 1, 1);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(pWindow))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(pWindow);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+    glClearColor(0, 1, 1, 1);
+    std::string vertexShader(vertex_shader);
+    std::string fragmentShader(fragment_shader);
+    Renderer::ShaderProgram shaderProgram(vertexShader, fragmentShader);
+    if (!shaderProgram.isCompiled()) {
+        std::cerr << "Can't create shader program!" << std::endl;
     }
 
+        GLuint points_vertex_buffer_object = 0;
+        glGenBuffers(1, &points_vertex_buffer_object);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vertex_buffer_object);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+        GLuint colors_vertex_buffer_object = 0;
+        glGenBuffers(1, &colors_vertex_buffer_object);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vertex_buffer_object);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+        GLuint vertex_attribute_object = 0;
+        glGenVertexArrays(1, &vertex_attribute_object);
+        glBindVertexArray(vertex_attribute_object);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vertex_buffer_object);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vertex_buffer_object);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+
+        /* Loop until the user closes the pWindow */
+        while (!glfwWindowShouldClose(pWindow))
+        {
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            shaderProgram.use(); 
+            glBindVertexArray(vertex_attribute_object);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(pWindow);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+    
     glfwTerminate();
-	 system("pause");
+    system("pause");
     return 0;
 }
+
