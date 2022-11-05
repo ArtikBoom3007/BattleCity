bf28676efe0f45510090c68db45b72c6a4b3f62e
Artem Kravch <krav514@gmail.com>
2022-11-05 23:36:51 +0300

    glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include<iostream>
 
 int main(void)
 {
@@ -18,12 +21,21 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+	
+	if (!gladLoadGL()) {
+        std::cout << "Can't load GLAD!" << std::endl;
+        return -1;
+    }
+    std::cout << "Renderer : " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version : " << glGetString(GL_VERSION) << std::endl;
+	
+	glClearColor(0, 1, 1, 1);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        // glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
