0b2185bf7679154a4d0c75c769d22b74e8d4d656
Artem Kravch <krav514@gmail.com>
2022-11-06 01:57:24 +0300

    textures 2

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -1,5 +1,6 @@
 #include "ResourceManager.h"
 #include "../Renderer/ShaderProgram.h"
+#include "../Renderer/Texture2D.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_ONLY_PNG
@@ -62,7 +63,7 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::getShaderProgram(const
 	return nullptr;
 }
 
-void ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath) {
+std::shared_ptr<Renderer::Texture2D> ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath) {
 	int channels = 0;
 	int width = 0;
 	int height = 0;
@@ -70,6 +71,25 @@ void ResourceManager::loadTexture(const std::string& textureName, const std::str
 	unsigned char* pixels = stbi_load(std::string(m_path + "/" + texturePath).c_str(), &width, &height, &channels, 0);
 	if (!pixels) {
 		std::cerr << "Can't load image: " << textureName << std::endl;
+		return nullptr;
 	}
+	std::shared_ptr<Renderer::Texture2D> newTexture = m_textures.emplace(textureName, std::make_shared<Renderer::Texture2D>(width, 
+																																	  height,
+																																	  pixels,
+																																	  channels, 
+																																	    GL_NEAREST, 
+																																		GL_CLAMP_TO_EDGE)).first->second;
+
 	stbi_image_free(pixels);
+	return newTexture;
+}
+
+std::shared_ptr<Renderer::Texture2D> ResourceManager::getTexture(const std::string& textureName) {
+	TexturesMap::const_iterator it = m_textures.find(textureName);
+	if (it != m_textures.end()) {
+		return it->second;
+	}
+	std::cerr << "Can't find the texture: " << textureName << std::endl;
+	return nullptr;
+
 }
