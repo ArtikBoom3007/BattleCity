badc505b0c229db0acaedf390f0f143cb26037a6
Artem Kravch <krav514@gmail.com>
2022-11-07 17:47:14 +0300

    Spites on load

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "Renderer/ShaderProgram.h"
 #include "Resources/ResourceManager.h"
 #include "Renderer/Texture2D.h"
+#include "Renderer/Sprite.h"
 
 glm::ivec2 g_windowSize(640, 400);
 
@@ -88,8 +89,17 @@ int main(int argc, char** argv)
             return -1;
         }
 
+        auto pSpriteShaderProgram = resourceManager.loadShaders("SpriteShader", "res/shaders/vSprite.txt", "res/shaders/fSprite.txt");
+        if (!pSpriteShaderProgram) {
+            std::cerr << "Can't create shader program: " << "SpriteShader " << std::endl;
+            return -1;
+        }
+
         auto tex = resourceManager.loadTexture("DefaultTexture", "res/textures/pngegg.png");
-        
+
+        auto pSprite = resourceManager.loadSprite("NewSprite", "DefaultTexture", "SpriteShader", 50, 100);
+        pSprite->setPosition(glm::vec2(300, 100));
+
         GLuint points_vertex_buffer_object = 0;
         glGenBuffers(1, &points_vertex_buffer_object);
         glBindBuffer(GL_ARRAY_BUFFER, points_vertex_buffer_object);
@@ -133,7 +143,11 @@ int main(int argc, char** argv)
         glm::mat4 projectionMatrix = glm::ortho(0.f, static_cast<float>(g_windowSize.x), 0.f,static_cast<float>(g_windowSize.y), -100.f, 100.f);
 
         pDefaultShaderProgram->setMatrix4("projectionMat", projectionMatrix);
-    
+        
+        pSpriteShaderProgram->use();
+        pSpriteShaderProgram->setInt("tex", 0);
+
+        pSpriteShaderProgram->setMatrix4("projectionMat", projectionMatrix);
 
         /* Loop until the user closes the pWindow */
         while (!glfwWindowShouldClose(pWindow))
@@ -150,6 +164,8 @@ int main(int argc, char** argv)
 
             pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_2);
             glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            pSprite->render();
 
             /* Swap front and back buffers */
             glfwSwapBuffers(pWindow);
